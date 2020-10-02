2662532e72f366634b83233f8ec6d3530a74668c
dmitryrocha <dmitryrocha@gmail.com>
2020-10-02 16:31:05 -0300

    Criado um card reusável

--- lib/main.dart
+++ lib/main.dart
@@ -24,12 +24,65 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: CardReusavel(
+                  cor: Color(0xFF1D1E33),
+                ),
+              ),
+              Expanded(
+                child: CardReusavel(
+                  cor: Color(0xFF1D1E33),
+                ),
+              ),
+            ],
+          )),
+          Expanded(
+            child: CardReusavel(
+              cor: Color(0xFF1D1E33),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: CardReusavel(
+                    cor: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: CardReusavel(
+                    cor: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+    );
+  }
+}
+
+class CardReusavel extends StatelessWidget {
+  CardReusavel({@required this.cor});
+
+  Color cor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: cor,
+        borderRadius: BorderRadius.circular(10.0),
       ),
+      height: 200.0,
+      width: 170.0,
     );
   }
 }
